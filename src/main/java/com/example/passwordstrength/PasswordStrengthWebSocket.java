c0190e051c210e54664a5a9467b75fcfb9ed911d
Faisal Ahsan <f.ahsan.07@gmail.com>
2023-07-07 16:07:08 +0100

    Refactor PasswordStrengthWebsocket to only use Spring imports

--- src/main/java/com/example/passwordstrength/PasswordStrengthWebSocket.java
+++ src/main/java/com/example/passwordstrength/PasswordStrengthWebSocket.java
@@ -1,49 +1,43 @@
 package com.example.passwordstrength;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.websocket.OnMessage;
-import jakarta.websocket.Session;
-import jakarta.websocket.server.ServerEndpoint;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
 
+import java.io.IOException;
+
 @Component
-@ServerEndpoint("/api/password/strength")
 public class PasswordStrengthWebSocket implements WebSocketHandler {
 
-    @OnMessage
-    public void onMessage(Session session, String message) {
-        int passwordStrength = calculatePasswordStrength(message);
+    @Override
+    public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws IOException {
+        String password = message.getPayload().toString();
+        int passwordStrength = calculatePasswordStrength(password);
 
         // Send the password strength back to the client
-        session.getAsyncRemote().sendText(Integer.toString(passwordStrength));
+        session.sendMessage(new TextMessage(Integer.toString(passwordStrength)));
     }
 
     private int calculatePasswordStrength(String password) {
-        // Implement password strength calculation logic here
-        return 0;
+        // Implement password strength calculation logic here (score out of 100)
+        return password.length();
     }
 
     @Override
-    public void afterConnectionEstablished(@NotNull WebSocketSession session) throws Exception {
-
-    }
-
-    @Override
-    public void handleMessage(@NotNull WebSocketSession session, @NotNull WebSocketMessage<?> message) throws Exception {
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
 
     }
 
     @Override
-    public void handleTransportError(@NotNull WebSocketSession session, @NotNull Throwable exception) throws Exception {
+    public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
 
     }
 
     @Override
-    public void afterConnectionClosed(@NotNull WebSocketSession session, @NotNull CloseStatus closeStatus) throws Exception {
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus closeStatus) throws Exception {
 
     }
 
